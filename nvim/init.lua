9411fdc36079ccd07d90515b5d31d16163d7a91d
Mykola <mykkola@gmail.com>
2023-11-20 14:36:25 +0200

    disable move & swap, too unnecessary

--- nvim/init.lua
+++ nvim/init.lua
@@ -469,35 +469,8 @@ vim.defer_fn(function()
           ['ic'] = '@class.inner',
         },
       },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
-      },
+      move = { enable = false },
+      swap = { enable = false },
     },
   }
 end, 0)
