6dd136fc64d47d1a92768e028dc5a922c043e49c
mykkola <mykkola@gmail.com>
2024-11-18 18:58:50 +0200

    gp

--- nvim/init.lua
+++ nvim/init.lua
@@ -304,6 +304,14 @@ require('lazy').setup({
     event = "InsertEnter",
     opts = {}
   },
+  {
+    "robitx/gp.nvim",
+    config = function()
+      require("gp").setup({
+        openai_api_key = { "pass", "show", "openai-first" },
+      })
+    end,
+  }
 }, {})
 
 
@@ -742,5 +750,75 @@ end
 
 vim.opt.spelllang = 'en_us'
 vim.keymap.set({ 'n', 'v', 'i' }, '<F7>', '<esc>:set invspell<cr>')
+
+require("which-key").add({
+  {
+    mode = { "v" },
+    nowait = true,
+    remap = false,
+    { "<C-g><C-t>", ":<C-u>'<,'>GpChatNew tabnew<cr>",   desc = "ChatNew tabnew" },
+    { "<C-g><C-v>", ":<C-u>'<,'>GpChatNew vsplit<cr>",   desc = "ChatNew vsplit" },
+    { "<C-g><C-x>", ":<C-u>'<,'>GpChatNew split<cr>",    desc = "ChatNew split" },
+    { "<C-g>a",     ":<C-u>'<,'>GpAppend<cr>",           desc = "Visual Append (after)" },
+    { "<C-g>b",     ":<C-u>'<,'>GpPrepend<cr>",          desc = "Visual Prepend (before)" },
+    { "<C-g>c",     ":<C-u>'<,'>GpChatNew<cr>",          desc = "Visual Chat New" },
+    { "<C-g>g",     group = "generate into new .." },
+    { "<C-g>ge",    ":<C-u>'<,'>GpEnew<cr>",             desc = "Visual GpEnew" },
+    { "<C-g>gn",    ":<C-u>'<,'>GpNew<cr>",              desc = "Visual GpNew" },
+    { "<C-g>gp",    ":<C-u>'<,'>GpPopup<cr>",            desc = "Visual Popup" },
+    { "<C-g>gt",    ":<C-u>'<,'>GpTabnew<cr>",           desc = "Visual GpTabnew" },
+    { "<C-g>gv",    ":<C-u>'<,'>GpVnew<cr>",             desc = "Visual GpVnew" },
+    { "<C-g>i",     ":<C-u>'<,'>GpImplement<cr>",        desc = "Implement selection" },
+    { "<C-g>n",     "<cmd>GpNextAgent<cr>",              desc = "Next Agent" },
+    { "<C-g>p",     ":<C-u>'<,'>GpChatPaste<cr>",        desc = "Visual Chat Paste" },
+    { "<C-g>r",     ":<C-u>'<,'>GpRewrite<cr>",          desc = "Visual Rewrite" },
+    { "<C-g>s",     "<cmd>GpStop<cr>",                   desc = "GpStop" },
+    { "<C-g>t",     ":<C-u>'<,'>GpChatToggle popup<cr>", desc = "Visual Toggle Chat" },
+  },
+  {
+    mode = { "n" },
+    nowait = true,
+    remap = false,
+    { "<C-g><C-t>", "<cmd>GpChatNew tabnew<cr>",   desc = "New Chat tabnew" },
+    { "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>",   desc = "New Chat vsplit" },
+    { "<C-g><C-x>", "<cmd>GpChatNew split<cr>",    desc = "New Chat split" },
+    { "<C-g>a",     "<cmd>GpAppend<cr>",           desc = "Append (after)" },
+    { "<C-g>b",     "<cmd>GpPrepend<cr>",          desc = "Prepend (before)" },
+    { "<C-g>c",     "<cmd>GpChatNew<cr>",          desc = "New Chat" },
+    { "<C-g>f",     "<cmd>GpChatFinder<cr>",       desc = "Chat Finder" },
+    { "<C-g>g",     group = "generate into new .." },
+    { "<C-g>ge",    "<cmd>GpEnew<cr>",             desc = "GpEnew" },
+    { "<C-g>gn",    "<cmd>GpNew<cr>",              desc = "GpNew" },
+    { "<C-g>gp",    "<cmd>GpPopup<cr>",            desc = "Popup" },
+    { "<C-g>gt",    "<cmd>GpTabnew<cr>",           desc = "GpTabnew" },
+    { "<C-g>gv",    "<cmd>GpVnew<cr>",             desc = "GpVnew" },
+    { "<C-g>n",     "<cmd>GpNextAgent<cr>",        desc = "Next Agent" },
+    { "<C-g>r",     "<cmd>GpRewrite<cr>",          desc = "Inline Rewrite" },
+    { "<C-g>s",     "<cmd>GpStop<cr>",             desc = "GpStop" },
+    { "<C-g>t",     "<cmd>GpChatToggle popup<cr>", desc = "Toggle Chat" },
+  },
+  {
+    mode = { "i" },
+    nowait = true,
+    remap = false,
+    { "<C-g><C-t>", "<cmd>GpChatNew tabnew<cr>",   desc = "New Chat tabnew" },
+    { "<C-g><C-v>", "<cmd>GpChatNew vsplit<cr>",   desc = "New Chat vsplit" },
+    { "<C-g><C-x>", "<cmd>GpChatNew split<cr>",    desc = "New Chat split" },
+    { "<C-g>a",     "<cmd>GpAppend<cr>",           desc = "Append (after)" },
+    { "<C-g>b",     "<cmd>GpPrepend<cr>",          desc = "Prepend (before)" },
+    { "<C-g>c",     "<cmd>GpChatNew<cr>",          desc = "New Chat" },
+    { "<C-g>f",     "<cmd>GpChatFinder<cr>",       desc = "Chat Finder" },
+    { "<C-g>g",     group = "generate into new .." },
+    { "<C-g>ge",    "<cmd>GpEnew<cr>",             desc = "GpEnew" },
+    { "<C-g>gn",    "<cmd>GpNew<cr>",              desc = "GpNew" },
+    { "<C-g>gp",    "<cmd>GpPopup<cr>",            desc = "Popup" },
+    { "<C-g>gt",    "<cmd>GpTabnew<cr>",           desc = "GpTabnew" },
+    { "<C-g>gv",    "<cmd>GpVnew<cr>",             desc = "GpVnew" },
+    { "<C-g>n",     "<cmd>GpNextAgent<cr>",        desc = "Next Agent" },
+    { "<C-g>r",     "<cmd>GpRewrite<cr>",          desc = "Inline Rewrite" },
+    { "<C-g>s",     "<cmd>GpStop<cr>",             desc = "GpStop" },
+    { "<C-g>t",     "<cmd>GpChatToggle popup<cr>", desc = "Toggle Chat" },
+  },
+})
 
 -- vim: ts=2 sts=2 sw=2 et
